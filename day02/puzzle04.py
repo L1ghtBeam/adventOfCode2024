a7f60f08985d844ad12d96573d20d17afd97ca2c
Colin Rodriguez <lightbeam7117@gmail.com>
2024-12-03 17:05:32 -0800

    Day 2

--- day02/puzzle04.py
+++ day02/puzzle04.py
@@ -1,50 +1,51 @@
-input_file = r'input_short.txt'
+input_file = r'input.txt'
+
+def valid_report(report: list, start: int, allowed_removals: int, sign: int) -> bool:
+    for i in range(start+1, len(report)):
+        diff = (report[i] - report[i-1]) * sign
+        if 1 <= diff <= 3:
+            continue
+
+        if not allowed_removals: return False
+        # check if we can keep i by removing i-1
+        if i > 1 and 1 <= (report[i] - report[i-2]) * sign <= 3:
+            if valid_report(report, i, allowed_removals - 1, sign):
+                return True
+
+        # check if we can remove i
+        if i + 1 < len(report) and 1 <= (report[i+1] - report[i-1]) * sign <= 3:
+            if valid_report(report, i+1, allowed_removals - 1, sign):
+                return True
+
+        # if we're at the start, then just remove it and keep going
+        if i == 1 and allowed_removals:
+            return valid_report(report, 1, allowed_removals - 1, sign)
+
+        # if we're at the last and have a removal remaining, then just remove it
+        if i == len(report) - 1 and allowed_removals:
+            return True
+
+        # we cannot remove anything, so impossible
+        return False
+
+    return True
+
+
 
 safe_reports = 0
 with open(input_file) as file:
     reports_as_chars = (line.split() for line in file)
     reports = ([int(x) for x in rep] for rep in reports_as_chars)
+    # reports_sloped = ([b-a for a, b in pairwise(report)] for report in reports)
 
     for report in reports:
         if len(report) == 1:
             safe_reports += 1
             continue
 
-        # try a monotonic increasing stack
-        s = []
-        removals = 0
-        for x in report:
-            safe = True
-            while s and (x <= s[-1] or abs(x - s[-1]) > 3):
-                s.pop()
-                removals += 1
-                if removals >= 2:
-                    safe = False
-                    break
-            s.append(x)
-
-            if not safe:
-                break
-        else:
+        if (valid_report(report, 0, 1, 1) or
+                valid_report(report, 0, 1, -1)):
             safe_reports += 1
-            break
 
-        # try a monotonic decreasing stack
-        s.clear()
-        removals = 0
-        for x in report:
-            safe = True
-            while s and (x >= s[-1] or abs(x - s[-1]) > 3):
-                s.pop()
-                removals += 1
-                if removals >= 2:
-                    safe = False
-                    break
-            s.append(x)
-
-            if not safe:
-                break
-        else:
-            safe_reports += 1
 
 print(safe_reports)
