aa1c829fb7b7b3faacf3c76592e713fb28e92fe9
Colin Rodriguez <lightbeam7117@gmail.com>
2025-04-13 16:03:04 -0700

    Day 21

--- day21/puzzle42.py
+++ day21/puzzle42.py
@@ -1,36 +1,33 @@
 import string
 from collections import deque, defaultdict
-from typing import NamedTuple, Iterator, Callable
+from typing import Callable, Iterator
 
-
-class Node(NamedTuple):
-    row: int
-    col: int
+from puzzle41 import Node, matrix_to_map, keypad, dpad
 
 class Keypad:
-    def __init__(self, button_map: dict[str, Node], subsequence_func: Callable[[str], str]) -> None:
+    def __init__(self, button_map: dict[str, Node], subsequence_func: Callable[[str], int]) -> None:
         self.button_map = button_map
 
         self.valid_positions = {val for val in button_map.values()}
-        self.path_cache: dict[tuple[Node, Node], str] = {}
+        self.path_cache: dict[tuple[Node, Node], int] = {}
 
         self.encode_subsequence = subsequence_func
 
-    def encode(self, sequence: str) -> str:
+    def encode(self, sequence: str) -> int:
         i = 0
-        out = []
+        out = 0
         start = self.button_map['A']
         while i < len(sequence):
             goal = sequence[i]
             end = self.button_map[goal]
 
-            out.append(self.find_path(start, end))
+            out += self.find_path(start, end)
 
             start = end
             i += 1
-        return ''.join(out)
+        return out
 
-    def find_path(self, start: Node, end: Node) -> str:
+    def find_path(self, start: Node, end: Node) -> int:
         if (start, end) in self.path_cache:
             return self.path_cache[(start, end)]
 
@@ -77,19 +74,10 @@ class Keypad:
         rec(end)
 
         # determine the best path by encoding the found sequences
-        best_path = None
-        best_path_len = float('inf')
-        for path in paths:
-            full_path = self.encode_subsequence(path)
-            if len(full_path) < best_path_len:
-                best_path_len = len(full_path)
-                best_path = full_path
+        best_path_len = min(self.encode_subsequence(path) for path in paths)
 
-        if best_path is None:
-            raise RuntimeError("No path found")
-
-        self.path_cache[(start, end)] = best_path
-        return best_path
+        self.path_cache[(start, end)] = best_path_len
+        return best_path_len
 
     @staticmethod
     def adjacent(node: Node) -> Iterator[tuple[Node, str]]:
@@ -98,52 +86,30 @@ class Keypad:
         yield Node(row=node.row, col=node.col - 1), '<'
         yield Node(row=node.row, col=node.col + 1), '>'
 
-def matrix_to_map(matrix: list[list[str | None]]) -> dict[str, Node]:
-    mapping = {}
-    for row in range(len(matrix)):
-        for col in range(len(matrix[row])):
-            if matrix[row][col] is None:
-                continue
-            mapping[matrix[row][col]] = Node(row=row, col=col)
-    return mapping
-
-keypad = [
-    ['7', '8', '9'],
-    ['4', '5', '6'],
-    ['1', '2', '3'],
-    [None, '0', 'A']
-]
-
-dpad = [
-    [None, '^', 'A'],
-    ['<', 'v', '>']
-]
-
 def main(path: str) -> int:
     keypad_map = matrix_to_map(keypad)
     dpad_map = matrix_to_map(dpad)
 
-    human_dpad = lambda seq: seq
-    freezing_dpad = Keypad(dpad_map, human_dpad)
-    radiation_dpad = Keypad(dpad_map, freezing_dpad.encode)
-    door_keypad = Keypad(keypad_map, radiation_dpad.encode)
+    prev = len
+    for _ in range(25):
+        directional_pad = Keypad(dpad_map, prev)
+        prev = directional_pad.encode
+
+    door_keypad = Keypad(keypad_map, prev)
 
     complexity = 0
     with open(path) as f:
         for line in f:
             sequence = line.strip()
-            print(f"{sequence}: ", end='')
 
-            code = door_keypad.encode(sequence)
-            print(code)
+            print(f"{sequence}: ",end='')
+            code_len = door_keypad.encode(sequence)
+            print(code_len)
 
             numeric = sequence.translate(str.maketrans('', '', string.ascii_letters))
-            complexity += len(code) * int(numeric)
+            complexity += code_len * int(numeric)
 
     return complexity
 
 if __name__ == '__main__':
-    print("Example:")
-    print(main('example.txt'))
-    print("\nInput:")
     print(main('input.txt'))
